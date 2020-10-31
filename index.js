aab66ef7b10994969376cf97b7786c40d9ae5ede
Henry Griffiths <henry@hgriffiths.net>
2020-10-31 03:12:35 -0400

    feat: create action

---
+++ index.js
@@ -0,0 +1,17 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+    const jirakey = core.getInput('jirakey')
+    const pattern = '^(build|ci|chore|docs|feat|fix|perf|refactor|revert|style|test)(.*)(:\\s)(' + jirakey + '-\\d+\\s*)+(:)'
+    const regex = new RegExp(pattern)
+    if (github.context.payload && github.context.payload.pull_request) {
+        title = github.context.payload.pull_request.title
+    }
+    core.info(title)
+    if (!(regex.test(title))) {
+        core.setFailed('PR Title must follow the format: type(scope?): ' + jirakey + '-123:. (The Jira Issue num may be repeated multiple times if needed).')
+    }
+} catch (error) {
+    core.setFailed(error)
+}
