05e7ecafad387c49166a8686a7ae86d6028ba1de
Henry Griffiths <henry@hgriffiths.net>
2020-10-31 03:44:07 -0400

    feat: allow none if no jira key exists

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const github = require('@actions/github');
 
 try {
     const jirakey = core.getInput('jirakey')
-    const pattern = '^(build|ci|chore|docs|feat|fix|perf|refactor|revert|style|test)(.*)(:\\s)(' + jirakey + '-\\d+\\s*)+(:)'
+    const pattern = '^(build|ci|chore|docs|feat|fix|perf|refactor|revert|style|test)(.*)(:\\s)(' + jirakey + '-\\d+\\s*|none)+(:)'
     const regex = new RegExp(pattern)
     if (github.context.payload && github.context.payload.pull_request) {
         title = github.context.payload.pull_request.title
