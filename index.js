970ba556a3af61c655340780158a085870d17cba
Henry Griffiths <42880130+henrygriffiths@users.noreply.github.com>
2021-12-13 09:50:55 -0600

    chore: change issue variable name (#6)
    * chore: change issue variable name
    
    * Update action.yml

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-    const jirakey = core.getInput('jirakey').toLowerCase().replace(/ /g, '')
-    const pattern = '^(build|ci|chore|docs|feat|fix|perf|refactor|revert|style|task|test)(.*)(:\\s)(' + jirakey + '-\\d+\\s*|none)+(:)'
+    const issuekey = core.getInput('issuekey').toLowerCase().replace(/ /g, '')
+    const pattern = '^(build|ci|chore|docs|feat|fix|perf|refactor|revert|style|task|test)(.*)(:\\s)(' + issuekey + '-\\d+\\s*|none)+(:)'
     const regex = new RegExp(pattern)
     if (github.context.payload && github.context.payload.pull_request) {
         var title = github.context.payload.pull_request.title
@@ -11,7 +11,7 @@ try {
     }
     core.info(title)
     if (!(regex.test(title))) {
-        core.setFailed('PR Title must follow the format: type(scope?): ' + jirakey + '-123:. (The Jira Issue num may be repeated multiple times if needed).')
+        core.setFailed('PR Title must follow the format: type(scope?): ' + issuekey + '-123:. (The issue num may be repeated multiple times if needed).')
     }
 } catch (error) {
     core.setFailed(error)
