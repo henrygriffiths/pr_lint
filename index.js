3eca0992ba92431c4d7e52c60315bb6322ee8aca
Henry Griffiths <42880130+henrygriffiths@users.noreply.github.com>
2021-12-13 10:16:53 -0600

    feat: array of issue ids

--- index.js
+++ index.js
@@ -3,15 +3,22 @@ const github = require('@actions/github');
 
 try {
     const issuekey = core.getInput('issuekey').toLowerCase().replace(/ /g, '')
-    const pattern = '^(build|ci|chore|docs|feat|fix|perf|refactor|revert|style|task|test)(.*)(:\\s)(' + issuekey + '-\\d+\\s*|none)+(:)'
-    const regex = new RegExp(pattern)
-    if (github.context.payload && github.context.payload.pull_request) {
-        var title = github.context.payload.pull_request.title
-        title = title.toLowerCase()
+    const keyarr = jirakey.split(',')
+    var passed = false
+    for (let i = 0; i < keyarr.length; i++) {
+        const pattern = '^(build|ci|chore|docs|feat|fix|perf|refactor|revert|style|task|test)(.*)(:\\s)(' + issuekey + '-\\d+\\s*|none)+(:)'
+        const regex = new RegExp(pattern)
+        if (github.context.payload && github.context.payload.pull_request) {
+            var title = github.context.payload.pull_request.title
+            title = title.toLowerCase()
+        }
+        core.info(title)
+        if (regex.test(title)) {
+            passed = true
+        }
     }
-    core.info(title)
-    if (!(regex.test(title))) {
-        core.setFailed('PR Title must follow the format: type(scope?): ' + issuekey + '-123:. (The issue num may be repeated multiple times if needed).')
+    if (passed == false) {
+        core.setFailed('PR Title must follow the format: type(scope?): ' + issuekey.toString() + '-123:. (The issue num may be repeated multiple times if needed).')
     }
 } catch (error) {
     core.setFailed(error)
